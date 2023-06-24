06e7ab4026920d89f30d0891fd116f42dd59779f
navalraul <129669942+navalraul@users.noreply.github.com>
2023-06-24 22:53:45 +0530

    update

---
+++ counterapp/src/App.jsx
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Welcome from './components/Welcome';
+import Login from './components/Login';
+
+
+function App() {
+  return (
+    <div>
+
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element= { <Home /> }/>
+        <Route exact path="/welcome" element= { <Welcome/> }/>
+        <Route exact path='/login' element={ <Login /> } />
+      </Routes>
+
+      
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ counterapp/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Footer () {
+    return (
+        <div>
+            <h2>This is Footer </h2>
+        </div>
+    )
+}
+
+export default Footer;
